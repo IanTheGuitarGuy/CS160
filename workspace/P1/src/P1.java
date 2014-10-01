8ae9c3ddf091e0779964c93efb5b5903457429e4
Ian Arcuri <ian85@dover.cs.colostate.edu>
2014-10-01 16:13:44 -0600

    Add workspace/

---
+++ workspace/P1/src/P1.java
@@ -0,0 +1,18 @@
+// P1 Assignment
+// Author: Ian Arcuri
+// Date: Aug 28, 2014
+// Class: CS160
+// Email: arcuri.ian@gmail.com
+public class P1 {
+
+	public static void main(String[] args) {
+		System.out.println("Computer Science is fun!");
+		System.out.println("So is Java Programming.");
+		System.out.println("Arcuri, Ian");
+		System.out.println("Computer Science");
+		System.out.println("CS160 Foundations in Programming");
+		System.out.println("9/2/2014");
+		System.out.println("I am in computer science because I wish to have a more thorough and satisfying understanding of computing");
+		
+	}
+}
